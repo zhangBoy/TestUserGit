651474fba223ac514ef2107c9039c942a95e8522
zhangbiao <1549217659@qq.com>
2022-09-11 10:40:03 +0800

    create person class

---
+++ TestToUseGit/Person.h
@@ -0,0 +1,17 @@
+//
+//  Person.h
+//  TestToUseGit
+//
+//  Created by Zboy on 2022/9/11.
+//  Copyright © 2022 Zboy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestToUseGit/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  TestToUseGit
+//
+//  Created by Zboy on 2022/9/11.
+//  Copyright © 2022 Zboy. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
